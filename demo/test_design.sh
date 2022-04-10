2fdfb03feaba68b795b12e69ef1c700a5dd92c2e
Naoya Kobayashi <kobnaoya@gmail.com>
2022-04-10 23:04:03 +0900

    Update test_design.sh

--- demo/test_design.sh
+++ demo/test_design.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-../simplefastdesign.py -n 50 -p redesign_7ec8 -r ./7ec8A_mod.resfile -i 2 ./7ec8A.pdb
+### Requirement: pyrosetta already installed ###
+python3 ../simplefastdesign.py -n 50 -p redesign_7ec8 -r ./7ec8A_mod.resfile -i 2 ./7ec8A.pdb
